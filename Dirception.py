21724cbe9db38f5709fb1a932a6012a50e6242e4
BaconBizon <mahni16@student.sdu.dk>
2018-12-05 12:34:23 +0100

    Add files via upload

---
+++ Dirception.py
@@ -0,0 +1,21 @@
+import os
+import fileinput
+
+path = "C:/Users/Mathias Haugaard/Documents"
+
+print("How many directories do you want?")
+
+for line in fileinput.input():
+    for i in range(int(line)):
+        dir = "Dir" + str(i)
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+        
+        path += "/" + dir
+        try:
+            os.chdir(path)
+            
+        except (OSError):
+            pass
+
+    break
